97c7564c8a8bdbabf00c9b0be3a5d30de6ff5b32
DESKTOP-MUSV7IS\Francesco <francesco_brischetto@hotmail.it>
2019-07-15 12:59:01 +0200

    Fixed Names of variables and Comments

--- main.py
+++ main.py
@@ -1,73 +1,77 @@
 import PySimpleGUI as sg
 import pandas as pd
+import sys
 from pandas.core.frame import DataFrame
 from cryptography.fernet import Fernet
 
-global dataframe
-global dataframe2
-global rows_found, columns_found
-global columns_name
-global currpath
+#Global variables
+global mainData, auxiliarData, dataRowsFound, dataColumnsFound, dataColumnsName, dataCurrentPath, dataSavedPath
 
-
-'''
-Funzioni per Pseudonimizzare
-'''
-
-#Encrypt with secret key
+#Functions for Pseudonimize
+#
+#
+#
+#Encrypt with secret key function
 def EncryKey(myvalues):
     Lista = {}
     #filtro i valori in myvalues separando riga da modalita'
     for (key, value) in myvalues.items():
         if "id" in str(key) and value is True:
-            k1= columns_name[int(key[0])]
+            k1= dataColumnsName[int(key[0])]
             k2= key[1:]
             Lista[k1] = k2
     keys = list(map(lambda x: str(x), Lista.keys()))
-    dataframe2 = dataframe[keys]
+    auxiliarData = mainData[keys]
     #applico il metodo di Encryption
     for(key,value) in Lista.items():
-        for n in range(rows_found):
+        for n in range(dataRowsFound):
             if value=="id":
-                message = dataframe.loc[n,key].encode()
+                message = mainData.loc[n,key].encode()
                 k = Fernet.generate_key()
                 f = Fernet(k)
                 encrypted = f.encrypt(message)
-                dataframe.at[n,key] = encrypted.decode()
-                dataframe2.at[n,key] = k
-    dataframe.to_excel(myvalues['dirButton']+"\encryKeyfile.xlsx")
-    dataframe2.to_excel(myvalues['dirButton']+"\encryKeyOtherDatafile.xlsx")
+                mainData.at[n,key] = encrypted.decode()
+                auxiliarData.at[n,key] = k
+    mainData.to_excel(myvalues['dirButton']+"\encryKeyfile.xlsx")
+    auxiliarData.to_excel(myvalues['dirButton']+"\encryKeyOtherDatafile.xlsx")
     sg.Popup('File created correctly!') 
     return
-            
+
+#Hash function           
 def Hashfun():
     print('Hashfun')
 
+#Salted Hash function
 def SaltedHashfun():
     print('SaltedHashfun')
 
+#Keyed-hash function with stored key
 def KeyedHashfun():
     print('KeyedHashfun')
 
+#Deterministic Encryption
 def DetermEncry():
     print('DetermEncry')
 
+#Tokenization
 def Token():
     print('Token')
     
 
-
-#Layout della schermata di Upload
-layout = [
-    [sg.Text('Upload Excel File', size=(80,1)) , sg.FileBrowse(key='browseButton')],
-    [sg.Button('OK')]
+#Main Program
+#
+#
+#
+#Layout Menu Screen
+layoutMenuScreen = [
+    [sg.Text('Menu Screen:')],
+    [sg.Button('Pseudonimize', key='Pseudo_screen'),sg.Button('Reidentify', key='Reid_screen'),sg.Button('Exit', key='Exit_screen')]
     ]
 
-#Schermata di upload
-window = sg.Window('Pseudonomization', default_element_size=(120, 30)).Layout(layout)
+#Menu Screen
+windowMenuScreen = sg.Window('Pseudonomization Screen', default_element_size=(120, 30)).Layout(layoutMenuScreen)
 
-
-#Serve per fare lo switch-case tra le funzioni da chiamare
+#Switch-case vocabulary
 options = {"Encrypt with secret key"                : EncryKey,
            'Hash function'                          : Hashfun,
            'Salted Hash Function'                   : SaltedHashfun,
@@ -76,40 +80,81 @@ options = {"Encrypt with secret key"                : EncryKey,
            'Tokenization'                           : Token,
 }
 
-#Ciclo di eventi della schermata 1
+#Event Cicle Menu Screen
 while True:
-    button, values = window.Read()
-    if button is None :
-        break
-    if button is "OK":
-        #leggo i valori dal file excel caricandoli in un dataframe    
-        dataframe = pd.read_excel (values['browseButton'])
-        rows_found = dataframe.shape[0]
-        columns_found = dataframe.shape[1]
-        columns_name = list( dataframe.columns)
-        #Creo il layout della schermata di config
-        layout1 = [
-            [sg.Combo(('Encrypt with secret key', 'Hash function','Salted Hash Function','Keyed-hash function with stored key','Deterministic Encryption','Tokenization'), readonly=True , size=(60, 1))],
-            [sg.Text('Select the Directory to store results', size=(80,1)) , sg.FolderBrowse(key='dirButton')],
-            [sg.Button('Start'),sg.Button('Exit')]
+    buttonMenuScreen, valuesMenuScreen = windowMenuScreen.Read()
+    if buttonMenuScreen is None or buttonMenuScreen is "Exit_screen":
+        windowMenuScreen.Close()
+        sys.exit(0)
+    if buttonMenuScreen is "Pseudo_screen":
+        #Hide/Disable/Disappear Menu Screen
+        windowMenuScreen.Disable()
+        windowMenuScreen.Disappear()
+        windowMenuScreen.Hide()
+        #Layout Pseudo Screen
+        layoutPseudoScreen = [
+            [sg.Text('Encrypt Screen:')],
+            [sg.Text('Upload Excel File', size=(50,1)) , sg.FileBrowse(key='browseButton')],
+            [sg.Button('Menu', key='Menu_screen'),sg.Button('OK', key='OK_screen'),sg.Button('Exit', key='Exit_screen')]
             ]
-        #Aggiungo due radiobox per ogni colonna
-        for num in range(columns_found):
-            layout1.insert(1+num, [sg.Text(columns_name[num],size=(30,1)),sg.Radio('Identifier', size=(12, 1), key=str(num)+'id',group_id=num), sg.Radio('Non Identifier', size=(12, 1), key=str(num)+'nd', group_id=num, default=True)])
-        #Apro la nuova schermata    
-        window2 = sg.Window('Pseudonomization',default_element_size=(120, 30)).Layout(layout1)
-        #Chiudo la schermata precedente
-        window.Close()
-        #Ciclo di eventi della schermata 2
+        
+        #Pseudo Screen
+        windowPseudoScreen = sg.Window('Pseudonomization Screen', default_element_size=(120, 30)).Layout(layoutPseudoScreen)
+        #Event Cicle Pseudo Screen
         while True:
-            button2,val2 = window2.Read()
-            if button2 is None or button2 is "Exit":
+            buttonPseScreen, valuesPseScreen = windowPseudoScreen.Read()
+            if buttonPseScreen is None or buttonPseScreen is "Exit_screen":
+                windowPseudoScreen.Close()
+                windowMenuScreen.Close()
+                sys.exit(0)
+            if buttonPseScreen is "Menu_screen":
+                #Enable/Reappear/UnHide Menu Screen (back to Menu)
+                windowPseudoScreen.Close()
+                windowMenuScreen.Enable()
+                windowMenuScreen.Reappear()
+                windowMenuScreen.UnHide()
                 break
-            if button2 is "Start":
-                #Richiamo lo switch-case passandogli i valori acquisiti dalla schermata 2
-                print(val2)
-                options[val2[0]](val2)         
-                window2.Close()
-                break
-        break
-print("Program Finished.")
+            if buttonPseScreen is "OK_screen":
+                try:
+                    mainData = pd.read_excel (valuesPseScreen['browseButton'])
+                    dataRowsFound = mainData.shape[0]
+                    dataColumnsFound = mainData.shape[1]
+                    dataColumnsName = list( mainData.columns)
+                    #Layout Config Pseudo Screen
+                    layoutConfigPseudoScreen = [
+                        [sg.Combo(('Encrypt with secret key', 'Hash function','Salted Hash Function','Keyed-hash function with stored key','Deterministic Encryption','Tokenization'), readonly=True , size=(60, 1))],
+                        [sg.Text('Select the Directory to store results', size=(80,1)) , sg.FolderBrowse(key='dirButton')],
+                        [sg.Button('Menu', key='Menu_screen'),sg.Button('Start',key='Start_screen'),sg.Button('Exit', key='Exit_screen')]
+                        ]
+                    #Dynamic Layout for each column found
+                    for i in range(dataColumnsFound):
+                        layoutConfigPseudoScreen.insert(1+i, [sg.Text(dataColumnsName[i],size=(30,1)),sg.Radio('Is Identifier', size=(12, 1), key=str(i)+'id',group_id=i), sg.Radio('Non Identifier', size=(12, 1), key=str(i)+'nd', group_id=i, default=True)])
+                    #Config Pseudo Screen 
+                    windowConfigPseudoScreen = sg.Window('Pseudonomization',default_element_size=(120, 30)).Layout(layoutConfigPseudoScreen)
+                    #Close Previous Screen
+                    windowPseudoScreen.Close()
+                    #Event Cicle Config Pseudo Screen
+                    while True:
+                        buttonConfPseScreen,valConfPseScreen = windowConfigPseudoScreen.Read()
+                        if buttonConfPseScreen is None or buttonConfPseScreen is "Exit_screen":
+                            windowMenuScreen.Close()
+                            windowConfigPseudoScreen.Close()
+                            sys.exit(0)
+                        if buttonConfPseScreen is "Menu_screen":
+                            #Enable/Reappear/UnHide Menu Screen (back to Menu)
+                            windowConfigPseudoScreen.Close()
+                            windowMenuScreen.Enable()
+                            windowMenuScreen.Reappear()
+                            windowMenuScreen.UnHide()
+                            break
+                        if buttonConfPseScreen is "Start_screen":
+                            #Choose a function to call
+                            options[valConfPseScreen[0]](valConfPseScreen)    
+                            windowMenuScreen.Close()     
+                            windowConfigPseudoScreen.Close()
+                            sys.exit(0)
+                    break
+                except SystemExit:
+                    sys.exit(0)
+                except:
+                    sg.PopupError("Error! Please upload a correct Excel File!")
